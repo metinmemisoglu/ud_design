2b16060a9588dcac1ae9eda13bd7436954dffd27
metinmemisoglu <metinmemisoglu@gmail.com>
2014-10-01 22:52:13 +0300

    metinm : builder

--- ud_design_denemeler/src/builder/BuilderClient.java
+++ ud_design_denemeler/src/builder/BuilderClient.java
@@ -3,8 +3,18 @@ package builder;
 public class BuilderClient {
 
 	public static void main(String[] args) {
-		
+		QueryBuilderDirector director = new QueryBuilderDirector();
+		String from = "from...";
+		String where = "where";
 
+		QueryBuilder qb = new SqlQueryBuilder();
+		Query q = director.buildQuery(from, where, qb);
+		q.execute();
+		
+		qb = new MongoQueryBuilder();
+		q = director.buildQuery(from, where, qb);
+		q.execute();
+		
 	}
 
 }
